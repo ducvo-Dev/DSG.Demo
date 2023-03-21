c93139b7698ee6ece5643e173ff6ad81da32330c
taduc333 <58924953+taduc333@users.noreply.github.com>
2023-03-22 00:48:49 +0700

    Implement Service for Business Logic Layer

---
+++ DSG.Service/ApplicationRoleService.cs
@@ -0,0 +1,109 @@
+﻿using DSG.Common.Exceptions;
+using DSG.Data.Infrastructure;
+using DSG.Data.Repositories;
+using DSG.Model.Models;
+using System.Collections.Generic;
+using System.Linq;
+
+
+
+namespace DSG.Service
+{
+    public interface IApplicationRoleService
+    {
+        AppRole GetDetail(string id);
+
+        IEnumerable<AppRole> GetAll(int page, int pageSize, out int totalRow, string filter);
+
+        IEnumerable<AppRole> GetAll();
+
+        AppRole Add(AppRole appRole);
+
+        void Update(AppRole AppRole);
+
+        void Delete(string id);
+
+        //Add roles to a sepcify group
+        bool AddRolesToGroup(IEnumerable<ApplicationRoleGroup> roleGroups, int groupId);
+
+        //Get list role by group id
+        IEnumerable<AppRole> GetListRoleByGroupId(int groupId);
+
+        void Save();
+    }
+
+    public class ApplicationRoleService : IApplicationRoleService
+    {
+        private IApplicationRoleRepository _appRoleRepository;
+        private IApplicationRoleGroupRepository _appRoleGroupRepository;
+        private IUnitOfWork _unitOfWork;
+
+        public ApplicationRoleService(IUnitOfWork unitOfWork,
+            IApplicationRoleRepository appRoleRepository, IApplicationRoleGroupRepository appRoleGroupRepository)
+        {
+            this._appRoleRepository = appRoleRepository;
+            this._appRoleGroupRepository = appRoleGroupRepository;
+            this._unitOfWork = unitOfWork;
+        }
+
+        public AppRole Add(AppRole appRole)
+        {
+            if (_appRoleRepository.CheckContains(x => x.Description == appRole.Description))
+                throw new NameDuplicatedException("Tên không được trùng");
+            return _appRoleRepository.Add(appRole);
+        }
+
+        public bool AddRolesToGroup(IEnumerable<ApplicationRoleGroup> roleGroups, int groupId)
+        {
+            _appRoleGroupRepository.DeleteMulti(x => x.GroupId == groupId);
+            foreach (var roleGroup in roleGroups)
+            {
+                _appRoleGroupRepository.Add(roleGroup);
+            }
+            return true;
+        }
+
+        public void Delete(string id)
+        {
+            _appRoleRepository.DeleteMulti(x => x.Id == id);
+        }
+
+        public IEnumerable<AppRole> GetAll()
+        {
+            return _appRoleRepository.GetAll();
+        }
+
+        public IEnumerable<AppRole> GetAll(int page, int pageSize, out int totalRow, string filter = null)
+        {
+            var query = _appRoleRepository.GetAll();
+            if (!string.IsNullOrEmpty(filter))
+                query = query.Where(x => x.Description.Contains(filter));
+
+            totalRow = query.Count();
+            return query.OrderBy(x => x.Description).Skip(page * pageSize).Take(pageSize);
+        }
+
+        public AppRole GetDetail(string id)
+        {
+            return _appRoleRepository.GetSingleByCondition(x => x.Id == id);
+        }
+
+        public void Save()
+        {
+            _unitOfWork.Commit();
+        }
+
+        public void Update(AppRole Approle)
+        {
+            if (_appRoleRepository.CheckContains(x => x.Description == Approle.Description && x.Id != Approle.Id))
+                throw new NameDuplicatedException("Tên không được trùng");
+            _appRoleRepository.Update(Approle);
+        }
+
+        public IEnumerable<AppRole> GetListRoleByGroupId(int groupId)
+        {
+            return _appRoleRepository.GetListRoleByGroupId(groupId);
+        }
+    }
+}
+
---
+++ DSG.Service/ProductService.cs
@@ -0,0 +1,210 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DSG.Data.Infrastructure;
+using DSG.Data.Repositories;
+using DSG.Model.Models;
+
+namespace DSG.Service
+{
+    public interface IProductService
+    {
+        Product Add(Product Product);
+
+        void Update(Product Product);
+
+        Product Delete(int id);
+
+        IEnumerable<Product> GetAll();
+
+        IEnumerable<Product> GetAll(int? categoryId);
+
+        IEnumerable<Product> GetLastest(int top);
+
+        IEnumerable<Product> GetHotProduct(int top);
+
+        IEnumerable<Product> GetListProductByCategoryIdPaging(int categoryId, int page, int pageSize, string sort, out int totalRow);
+
+        IEnumerable<Product> Search(string keyword, int page, int pageSize, string sort, out int totalRow);
+
+        IEnumerable<Product> GetListProduct(string keyword);
+
+        IEnumerable<Product> GetReatedProducts(int id, int top);
+
+        IEnumerable<string> GetListProductByName(string name);
+
+        Product GetById(int id);
+
+        void Save();
+
+        void IncreaseView(int id);
+        bool SellProduct(int productId, int quantity);
+
+
+    }
+
+    public class ProductService : IProductService
+    {
+        private IProductRepository _productRepository;
+        private IUnitOfWork _unitOfWork;
+
+        public ProductService(IProductRepository productRepository, IUnitOfWork unitOfWork)
+        {
+            this._productRepository = productRepository;
+
+            this._unitOfWork = unitOfWork;
+        }
+
+        public Product Add(Product Product)
+        {
+            var product = _productRepository.Add(Product);
+            _unitOfWork.Commit();
+           
+            return product;
+        }
+
+        public Product Delete(int id)
+        {
+            return _productRepository.Delete(id);
+        }
+
+        public IEnumerable<Product> GetAll()
+        {
+            return _productRepository.GetAll().OrderBy(x => x.ID); ;
+        }
+
+        public IEnumerable<Product> GetAll(int? categoryId)
+        {
+            var query = _productRepository.GetAll(new string[] { "ProductCategory"});
+
+
+            if (categoryId.HasValue)
+                query = query.Where(x => x.CategoryID == categoryId.Value);
+
+            return query;
+        }
+
+        public Product GetById(int id)
+        {
+            return _productRepository.GetSingleById(id);
+        }
+
+        public void Save()
+        {
+            _unitOfWork.Commit();
+        }
+
+        public void Update(Product Product)
+        {
+            _productRepository.Update(Product);
+           
+        }
+
+        public IEnumerable<Product> GetLastest(int top)
+        {
+            return _productRepository.GetMulti(x => x.Status).OrderByDescending(x => x.CreatedDate).Take(top);
+        }
+
+        public IEnumerable<Product> GetHotProduct(int top)
+        {
+            return _productRepository.GetMulti(x => x.Status && x.HotFlag == true).OrderByDescending(x => x.CreatedDate).Take(top);
+        }
+
+        public IEnumerable<Product> GetListProductByCategoryIdPaging(int categoryId, int page, int pageSize, string sort, out int totalRow)
+        {
+            var query = _productRepository.GetMulti(x => x.Status && x.CategoryID == categoryId);
+
+            switch (sort)
+            {
+                case "popular":
+                    query = query.OrderByDescending(x => x.ViewCount);
+                    break;
+
+                case "discount":
+                    query = query.OrderByDescending(x => x.PromotionPrice.HasValue);
+                    break;
+
+                case "price":
+                    query = query.OrderBy(x => x.Price);
+                    break;
+
+                default:
+                    query = query.OrderByDescending(x => x.CreatedDate);
+                    break;
+            }
+
+            totalRow = query.Count();
+
+            return query.Skip((page - 1) * pageSize).Take(pageSize);
+        }
+
+        public IEnumerable<string> GetListProductByName(string name)
+        {
+            return _productRepository.GetMulti(x => x.Status && x.Name.Contains(name)).Select(y => y.Name);
+        }
+
+        public IEnumerable<Product> Search(string keyword, int page, int pageSize, string sort, out int totalRow)
+        {
+            var query = _productRepository.GetMulti(x => x.Status && x.Name.Contains(keyword));
+
+            switch (sort)
+            {
+                case "popular":
+                    query = query.OrderByDescending(x => x.ViewCount);
+                    break;
+
+                case "discount":
+                    query = query.OrderByDescending(x => x.PromotionPrice.HasValue);
+                    break;
+
+                case "price":
+                    query = query.OrderBy(x => x.Price);
+                    break;
+
+                default:
+                    query = query.OrderByDescending(x => x.CreatedDate);
+                    break;
+            }
+
+            totalRow = query.Count();
+
+            return query.Skip((page - 1) * pageSize).Take(pageSize);
+        }
+
+        public IEnumerable<Product> GetReatedProducts(int id, int top)
+        {
+            var product = _productRepository.GetSingleById(id);
+            return _productRepository.GetMulti(x => x.Status && x.ID != id && x.CategoryID == product.CategoryID).OrderByDescending(x => x.CreatedDate).Take(top);
+        }
+
+        public void IncreaseView(int id)
+        {
+            var product = _productRepository.GetSingleById(id);
+            if (product.ViewCount.HasValue)
+                product.ViewCount += 1;
+            else
+                product.ViewCount = 1;
+        }
+
+        //Selling product
+        public bool SellProduct(int productId, int quantity)
+        {
+            var product = _productRepository.GetSingleById(productId);
+            //if (product.Quantity < quantity)
+            //    return false;
+            //product.Quantity -= quantity;
+            return true;
+        }
+        public IEnumerable<Product> GetListProduct(string keyword)
+        {
+            IEnumerable<Product> query;
+            if (!string.IsNullOrEmpty(keyword))
+                query = _productRepository.GetMulti(x => x.Name.Contains(keyword));
+            else
+                query = _productRepository.GetAll();
+            return query;
+        }
+    }
+}
