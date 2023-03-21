2bbf6bb06a3fa2015c7264a9e5d025a0a1f4cbef
taduc333 <58924953+taduc333@users.noreply.github.com>
2023-03-22 00:57:22 +0700

    Identity

---
+++ DSG.Identity/ApplicationUserStore.cs
@@ -0,0 +1,19 @@
+﻿using DSG.Data;
+using DSG.Model.Models;
+using Microsoft.AspNet.Identity.EntityFramework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace DSG.Identity
+{
+    public class ApplicationUserStore : UserStore<AppUser>
+    {
+        public ApplicationUserStore(DsgDbContext context) : base(context)
+        {
+        }
+    }
+}
